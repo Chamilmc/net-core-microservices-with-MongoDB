ff350659e7999b04c67f198b8763dbb0216a0741
Chamilmc <chamilmc@gmail.com>
2021-02-04 11:59:19 +0530

    Add files via upload

---
+++ net-core-microservices/src/Actio.Api/Handlers/ActivityCreatedHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Actio.Api.Models;
+using Actio.Api.Repositories;
+using Actio.Common.Events;
+
+namespace Actio.Api.Handlers
+{
+    public class ActivityCreatedHandler : IEventHandler<ActivityCreated>
+    {
+        private readonly IActivityRepository _activityRepository;
+
+        public ActivityCreatedHandler(IActivityRepository activityRepository)
+        {
+            _activityRepository = activityRepository;
+        }
+            
+        public async Task HandleAsync(ActivityCreated @event)
+        {
+            await _activityRepository.AddAsync(new Activity
+            {
+                Id = @event.Id,
+                UserId = @event.UserId,
+                Name = @event.Name,
+                Category = @event.Category,
+                CreatedAt = @event.CreatedAt,
+                Description = @event.Description
+            });
+            Console.WriteLine($"Activity created: {@event.Name}");
+        }
+    }
+}
